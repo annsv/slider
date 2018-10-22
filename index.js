f3b342705ea9dc4d2700ab7d52ab531ec65a0d64
Anna Sebova <ganna.sebova@gmail.com>
2018-10-22 17:00:26 +0300

    add direction check

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 class Slider{
+
   constructor(el){
     this.el = document.querySelector(el);
     this.init();
   }
   
   init(){
+
     this.navItems = this.el.querySelectorAll('.slider__navigation-item');
+    this.left = this.el.querySelector('.slider__arrows-left');
+    this.right = this.el.querySelector('.slider__arrows-right');   
     this.slideContainer = this.el.querySelector('.slider__container');
     this.bullets();
     this.leftArrow();
@@ -15,30 +19,37 @@ class Slider{
   bullets(){
     for(let i=0; i < this.navItems.length; i++){
       let navItem = this.navItems[i];
-      this.slide(navItem, 1);
+      this.slide(navItem);
     }
   }
   leftArrow(){
-    this.left = this.el.querySelectorAll('.slider__arrows-left');
-    this.slide(this.left, -1);
-    console.log("left");
+    this.slide(this.left,-1);
   }  
   rightArrow(){
-    this.right = this.el.querySelectorAll('.slider__arrows-right');    
-    this.slide(this.right, 1);
-    console.log("right");
+    this.slide(this.right,1);
   } 
   
-  slide( link,direction ){
+  slide( link,direction = 0 ){
     var self = this;
+    console.log(self);
+    var index = 0;
     link.addEventListener('click', function(){
-      var index = parseInt(link.getAttribute('data-index'), 10) + direction;      
+      if(link.getAttribute('data-index') !== null){
+        index = parseInt(link.getAttribute('data-index'), 10) + 1;    
+        console.log("bullets");
+      } else if(direction === 1){  
+        index++;
+        console.log("arrows",index);
+      } else{
+        index--;
+      }
       var activeSlide = self.el.querySelector('.slider__item:nth-child(' + index + ')');
       
       self.slideContainer.style.left = '-' + activeSlide.offsetLeft + 'px';
       
     })
   }
+    
   };
 
   //API
